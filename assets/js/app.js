bc8644854517f43abd1269777858bfe35d13a24b
Yelitza Cho <yelitza.choque@gmail.com>
2017-04-19 10:18:16 -0500

    Ejercicio terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,30 @@
-window.addEventListener( 'click', function (e){
+window.addEventListener( 'load', function (e){
 	e.preventDefault();
 
 	var contenedor = document.getElementById("contenedor");
 
+	var divFather = document.createElement('div');
+	var divChild = document.createElement('div');
+	var divGrandChild = document.createElement('div');
+	var divFather2 = document.createElement('div');
+	var divChild2 = document.createElement('div');
+	var divGrandChild2 = document.createElement('div');
+
+	divGrandChild.setAttribute('class','divGrandChild');
+	divChild.appendChild(divGrandChild);
+
+	divChild.setAttribute('class','divChild');
+	divFather.appendChild(divChild);
+
+	divFather.setAttribute('class','divFather');
+	contenedor.appendChild(divFather);
 	
+	divGrandChild2.setAttribute('class','divGrandChild2');
+	divChild2.appendChild(divGrandChild2);
 
+	divChild2.setAttribute('class','divChild2');
+	divFather2.appendChild(divChild2);
 
-
+	divFather2.setAttribute('class','divFather2');
+	contenedor.appendChild(divFather2);
 })
